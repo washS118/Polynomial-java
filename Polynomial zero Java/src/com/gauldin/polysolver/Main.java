6623259f96be28b308e4cd4c67eeda4290c2b46d
Lucas <lukegauldin93@gmail.cm>
2016-10-20 15:32:47 -0400

    Initial commit
    Created framework for all methods needed

---
+++ Polynomial zero Java/src/com/gauldin/polysolver/Main.java
@@ -0,0 +1,41 @@
+package com.gauldin.polysolver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static List<Integer> getEquation(){
+		//TODO Equation retrieval method
+		List<Integer> equation = new ArrayList<Integer>();
+		
+		return equation;
+	}
+	
+	private static List<Integer> factor(int num){
+		//TODO Get list of all factors of num
+		List<Integer> factors = new ArrayList<Integer>();
+		
+		return factors;
+	}
+	
+	private static List<Double> getRoots(List<Integer> factorOne, List<Integer> factor2){
+		//TODO Calculate the rational roots
+		List<Double> roots = new ArrayList<Double>();
+		
+		return roots;
+	}
+	
+	private static List<Double> zeroTest(List<Double> roots, List<Integer> equation){
+		//TODO Calculate zeros
+		List<Double> zeros = new ArrayList<Double>();
+		
+		return zeros;
+	}
+
+}
